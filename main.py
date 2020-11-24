6df2ed44bc3e8d71b333e3fdb4a1aaea0ace1d62
ecepanagiotisnt <pkaoukis.2001@yahoo.gr>
2020-11-24 16:30:07 +0000

    millerrabin

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from random import randint
 
-def FermatTest(n, k = 30):
+def MillerRabinTest(a, d, s, n):
+  x = pow(a,d,n)
+  if(x == 1 or x == n-1):
+    return True
+  for i in range (0,s-1):
+    x = x**2 % n
+    if(x == n-1):
+      return True
+  return False
+
+def MRTest(n, k = 30):
   if(n == 1):
     return False;
   if(n == 2 or n == 3):
     return True;
+
+  d = n - 1
+  s = 0
+  while(d%2==0):
+      d = d//2
+      s = s+1
+
+
   for i in range(0,k):
     a = randint(2,n-1)
-    if (pow(a,n-1,n) != 1): return False;
+    if (MillerRabinTest(a,d,s,n)==False): 
+      return False;
   return True;
 
 
